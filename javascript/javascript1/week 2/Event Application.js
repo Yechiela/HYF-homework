6756e36b0a30fcffaa5d07c7909235353001ab1a
Yechiela <46047861+Yechiela@users.noreply.github.com>
2019-03-16 00:52:44 +0100

    Delete Event Application.js

--- javascript/javascript1/week 2/Event Application.js
+++
@@ -1,9 +0,0 @@
-/*Event application*/
-function getEventWeekday(day) {
-    const today=new Date();
-    let nextday=new Date();
-    nextday.setDate(nextday.getDate()+day);
-    const eventDay=nextday.getDay();
-    const days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    return days[eventDay];
-}
